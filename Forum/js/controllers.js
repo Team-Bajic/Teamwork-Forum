410b46d68ef599cbcec8a35c73fa25712119d928
Gleomit <draychev95@mail.ru>
2015-04-16 21:29:27 +0300

    better controller implementation

--- Forum/js/controllers.js
+++ Forum/js/controllers.js
@@ -23,23 +23,27 @@ Forum.controllers = (function() {
 
 	var PageController = {
 		ShowMain: function() {
+			var controllerData = {};
+
 			Forum.data.Category.getAll()
 				.then(function(result) {
 					var categoriesData = JSON.parse(JSON.stringify(result.results));
+
+					controllerData.categoriesData = categoriesData;
+
+					return Forum.data.Question.getAll();
+				}).then(function(result){
+					var questionsData = JSON.parse(JSON.stringify(result.results));
+
 					var categoryView = new Forum.views.CategoryView();
+					var questionView = new Forum.views.QuestionView();
 
-					Forum.data.Question.getAll()
-					.then(function(result){
-						var questionsData = JSON.parse(JSON.stringify(result.results));
-						var questionView = new Forum.views.QuestionView();
-
-						categoryView.render('.section-container', categoriesData);
-						questionView.render('.large-9', questionsData);
-					});
+					categoryView.render('.section-container', controllerData.categoriesData);
+					questionView.render('.large-9', questionsData);
 				});
 		},
 		createQuestion: function(){
-			
+
 		}
 	};
 
