9025171747def826658de7eef1d07fee1895677b
Deivid Raychev <draychev95@mail.ru>
2015-04-23 00:24:36 +0300

    tinymce integration

--- Forum/js/views.js
+++ Forum/js/views.js
@@ -38,6 +38,17 @@ Forum.views = (function() {
 
     assignNewQuestionEvents();
 
+    tinymce.init({
+    selector: "textarea",
+    plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+    });
+
+
     function assignNewQuestionEvents() {
       $('.reveal-options-block').on('click', function (event) {
         $('div.options-block').removeClass('hide');
