fc6a2ae2eeeb4d19b301f7d54e328f0eff4463d9
Deivid Raychev <draychev95@mail.ru>
2015-04-27 01:48:27 +0300

    delete buttons depending on user role

--- Forum/js/config.js
+++ Forum/js/config.js
@@ -3,3 +3,11 @@ var Forum = Forum || {};
 Forum.config = {
 	questionsPerPage: 5
 };
+
+Handlebars.registerHelper('ifEquals', function(a, b, options) {
+  if (a === b) {
+    return options.fn(this);
+  }
+    
+  return options.inverse(this);
+});
