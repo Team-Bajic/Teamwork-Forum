efbe3b3f22c3431e03575f3a87f15268736b652c
OmniPot <r.conkov@gmail.com>
2015-04-28 00:22:17 +0300

    Cloud code moved

---
+++ Forum-Cloud-Code/cloud/main.js
@@ -0,0 +1,137 @@
+Parse.Cloud.define("test", function(request, response) {
+	var user = Parse.User.current();
+	if (user) {
+		response.success(user);
+	} else {
+		response.error();
+	}
+
+})
+
+Parse.Cloud.beforeDelete("Answer", function(request, response) {
+	var user = request.user;
+	if (user.get('role').id === "0cmOiSZe8k" || user.id === request.object.get('postedBy').id) {
+		var questionQuery = new Parse.Query("Question");
+		var userQuery = new Parse.Query(Parse.User);
+
+		questionQuery.equalTo("answers", request.object);
+		questionQuery.include("answers");
+
+		userQuery.equalTo("answers", request.object);
+		userQuery.include("answers");
+
+		questionQuery.each(function(question) {
+			question.remove("answers", request.object);
+			question.save();
+		}, {
+			success: function() {
+				userQuery.each(function(user) {
+					user.remove("answers", request.object);
+					user.save();
+				}, {
+					success: function() {
+						response.success();
+					},
+					error: function(error) {
+						response.error(error);
+					}
+				});
+			},
+			error: function(error) {
+				response.error(error);
+			}
+		});
+	} else {
+		response.error("Login first!");
+	}
+});
+
+Parse.Cloud.beforeDelete("Question", function(request, response) {
+	var user = request.user;
+	if (user.get('role').id === "0cmOiSZe8k" || user.id === request.object.get('postedBy').id) {
+		var categoryQuery = new Parse.Query("Category");
+		var userQuery = new Parse.Query(Parse.User);
+
+		categoryQuery.equalTo("questions", request.object);
+		categoryQuery.include("questions");
+
+		userQuery.equalTo("questions", request.object);
+		userQuery.include("questions");
+
+		categoryQuery.each(function(category) {
+			category.remove("questions", request.object);
+			category.save();
+		}, {
+			success: function() {
+				userQuery.each(function(user) {
+					user.remove("questions", request.object);
+					user.save();
+				}, {
+					success: function() {
+						response.success();
+					},
+					error: function(err) {
+						response.error(err);
+					}
+				});
+			},
+			error: function(err) {
+				response.error(err);
+			}
+		});
+	} else {
+		response.error("Login first!");
+	}
+});
+
+Parse.Cloud.afterDelete("Question", function(request, response) {
+	var user = request.user;
+	if (user.get('role').id === "0cmOiSZe8k" || user.id === request.object.get('postedBy').id) {
+		var query = new Parse.Query("Answer");
+
+		query.equalTo('question', request.object);
+
+		query.find({
+			success: function(answers) {
+				Parse.Object.destroyAll(answers, {
+					success: function() {},
+					error: function(error) {
+						response.error(error);
+					}
+				});
+			},
+			error: function(error) {
+				response.error(error);
+			}
+		});
+	} else {
+		response.error("Login first!");
+	}
+});
+
+Parse.Cloud.beforeDelete("Category", function(request, response) {
+	var user = request.user;
+	if (user.get('role').id === "0cmOiSZe8k") {
+		var query = new Parse.Query("Question");
+
+		query.equalTo('category', request.object);
+
+		query.find({
+			success: function(questions) {
+				Parse.Object.destroyAll(questions, {
+					success: function() {
+						response.success();
+					},
+					error: function(error) {
+						response.error(error);
+					}
+				});
+			},
+			error: function(error) {
+				response.error(error);
+			}
+		});
+	} else {
+		response.error("Login first!");
+	}
+});
